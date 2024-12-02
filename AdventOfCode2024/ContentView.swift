654508866b6b08cb946754f16202ef34ec691e55
Matthew Waller <hello@cephalopod.studio>
2024-12-01 18:32:08 -0600

    Initial Commit

---
+++ AdventOfCode2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AdventOfCode2024
+//
+//  Created by Matthew Waller on 12/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
